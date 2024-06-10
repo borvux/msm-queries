e426748ee8b700f1bb222a43d5a800fe04b2da72
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-10 21:01:58 +0000

    added the show details of the directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,15 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "directors_templates/view"})
   end
+
+  def show
+    id = params.fetch("id")
+
+    matching_director = Director.where({ :id => id})
+    @director = matching_director.at(0)
+
+    @matching_movies = Movie.where({ :director_id => id})
+
+    render({ :template => "directors_templates/show"})
+  end
 end
