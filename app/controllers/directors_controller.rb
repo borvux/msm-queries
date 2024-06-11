ca902920a645cee3295a27107c07c7ed0a1d4e84
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-06-11 16:19:13 +0000

    finished up the youngest and eldest pages

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -11,6 +11,24 @@ class DirectorsController < ApplicationController
 
     @matching_movies = Movie.where({ :director_id => director_id })
 
-    render({ :template => "directors_templates/show" })
+    render({ :template => "/directors_templates/show" })
+  end
+
+  def youngest
+    finding_youngest = Director.where.not({ :dob => nil }).order({ :dob => :desc })
+    @youngest = finding_youngest.at(0)
+    dob = @youngest.dob
+    @date_formatted = dob.strftime("%B %d, %Y")
+
+    render({ :template => "/directors_templates/youngest" })
+  end
+
+  def eldest
+    finding_eldest = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @eldest = finding_eldest.at(0)
+    dob = @eldest.dob
+    @date_formatted = dob.strftime("%B %d, %Y")
+
+    render({ :template => "/directors_templates/eldest" })
   end
 end
